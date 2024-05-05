abe448a3a051332a887d02fff271351839d3fcce
ndthangit <ndtducthang2004@gmail.com>
2024-05-05 20:30:52 +0700

    update simulation

--- Simulation/main.cpp
+++ Simulation/main.cpp
@@ -1,5 +1,7 @@
 #include <iostream>
 #include <fstream>
+#include <sstream>
+
 #include <set>
 #include <string>
 #include <vector>
@@ -13,48 +15,89 @@ int main()
 {
     // import data
     vector<TaskSet> taskSetList;
-    fstream input("BasicInput.txt");
+    std::ifstream file("RTaskSets.txt"); // Open file
     std::string line;
 
     if (file.is_open())
     {
         while (std::getline(file, line))
-        { // Đọc từng dòng của file
+        {
             std::stringstream ss(line);
-            std::string token;
+            std::string title;
+            std::getline(ss, title); // read "begin"
 
-            while (std::getline(ss, token, ','))
-            { // Tách dữ liệu bằng dấu phẩy
-                std::cout << token << std::endl;
+            std::getline(file, line);
+            std::stringstream ss2(line);
+            std::string numTasksStr;
+            std::getline(ss2, numTasksStr);
+            int numTasks = std::stoi(numTasksStr);
+
+            std::getline(file, line);
+            std::stringstream ss3(line);
+            std::string numProcessorsStr;
+            std::getline(ss3, numProcessorsStr);
+            int numProcessors = std::stoi(numProcessorsStr);
+
+            std::getline(file, line);
+            std::stringstream ss4(line);
+            std::string preemptive;
+            std::getline(ss4, preemptive);
+
+            TaskSet taskSet(numTasks, numProcessors);
+            for (int i = 0; i < numTasks; i++)
+            {
+                std::getline(file, line);
+                std:: string startTime;
+                std:: string computationTime;
+                std:: string softDeadline;
+                std:: string hardDeadline;
+                std:: string period;
+                std::stringstream ss5(line);
+                std::getline(ss5, startTime, ',');
+                std::getline(ss5, computationTime, ',');
+                std::getline(ss5, softDeadline, ',');
+                std::getline(ss5, hardDeadline, ',');
+                std::getline(ss5, period, ',');
+                Task task("Task" + std::to_string(i), std::stoi(startTime), std::stoi(computationTime), std::stoi(hardDeadline), std::stoi(period));
+                taskSet.addTask(task);
             }
+            taskSetList.push_back(taskSet);
         }
-        file.close(); // Đóng file
+
+        for (int i = 0; i < taskSetList.size(); i++)
+        {
+            TaskSet taskSet = taskSetList[i];
+            cout << "TaskSet " << i << endl;
+            cout << "NumTasks: " << taskSet.getNumTasks() << endl;
+            cout << "NumProcessors: " << taskSet.getNumProcessors() << endl;
+            cout << "CheckNumTasks: " << taskSet.getTasks().size() << endl;
+            
+        }
+
+        
     }
     else
     {
         std::cerr << "Unable to open file!" << std::endl;
     }
-
-    input.close();
-
     /*
      ghi thời gian chạy của mỗi taskset vào file csv
      dùng csv vẽ đồ thị miêu tả sự khác nhau khi sử dụng kỹ thuật phân loại yêu cầu  avf khi không sử dụng
     */
 
-    fstream output;
-    output.open("Result.csv", ios ::out);
-    for (int i = 0; i < taskSetList.size(); i++)
-    {
-        clock_t start = clock();
-        int theNumberOfTasksAcrossTheDeadline;
-        theNumberOfTasksAcrossTheDeadline = runEDF(taskSetList[i]);
-        clock_t end = clock();
-        double elapsed_ticks = difftime(end, start);
-        double milliseconds = elapsed_ticks * (1000.0 / CLOCKS_PER_SEC);
-        output << milliseconds << ",";
-    }
-    output << endl;
+    // fstream output;
+    // output.open("Result.csv", ios ::out);
+    // for (int i = 0; i < taskSetList.size(); i++)
+    // {
+    //     clock_t start = clock();
+    //     int theNumberOfTasksAcrossTheDeadline;
+    //     theNumberOfTasksAcrossTheDeadline = runEDF(taskSetList[i]);
+    //     clock_t end = clock();
+    //     double elapsed_ticks = difftime(end, start);
+    //     double milliseconds = elapsed_ticks * (1000.0 / CLOCKS_PER_SEC);
+    //     output << milliseconds << ",";
+    // }
+    // output << endl;
 
     // output.close();
     return 0;
--- Simulation/test.cpp
+++ Simulation/test.cpp
@@ -7,83 +7,96 @@
 
 #include "Task.h"
 #include "TaskSet.h"
-// #include "EdfAlgorithm.h"
 
 using namespace std;
 
 int main()
 {
-    // test the TaskSet class
+
     // TaskSet taskSet;
-    TaskSet taskSet;
 
-    // Add tasks to the multiset
-    Task task1("Task1", 0, 5, 10, 20);
-    Task task2("Task2", 0, 3, 8, 15);
-    Task task3("Task3", 0, 7, 12, 25);
-    Task task4("Task4", 0, 2, 8, 10);
-    Task task5("Task5", 0, 4, 10, 20);
+    // // Add tasks to the multiset
+    // Task task1("Task1", 0, 5, 10, 20);
+    // Task task2("Task2", 0, 3, 8, 15);
+    // Task task3("Task3", 0, 7, 12, 25);
+    // Task task4("Task4", 0, 2, 8, 10);
+    // Task task5("Task5", 0, 4, 10, 20);
 
-    taskSet.addTask(task1);
-    taskSet.addTask(task2);
-    taskSet.addTask(task3);
-    taskSet.addTask(task4);
-    taskSet.addTask(task5);
-
-    // cout << "period: " << task1.getPriorityLevel() << endl;
-
-    // // test all methods of TaskSet
-    // taskSet.printTaskSet();
-    // taskSet.removeTask();
-    // taskSet.printTaskSet();
-    // taskSet.removeTask();
-    // cout << "max: " << taskSet.getHighestPriorityTask().getPriorityLevel() << endl;
+    // taskSet.addTask(task1);
+    // taskSet.addTask(task2);
+    // taskSet.addTask(task3);
+    // taskSet.addTask(task4);
+    // taskSet.addTask(task5);
 
     cout << "data:" << endl;
 
     vector<TaskSet> taskSetList;
-    // fstream input("BasicInput.txt");
-    std::ifstream file("BasicInput.txt"); // Mở file
+    std::ifstream file("BasicInput.txt"); // Open file
 
     std::string line;
 
     if (file.is_open())
     {
         while (std::getline(file, line))
-        { // Đọc từng dòng của file
+        {
             std::stringstream ss(line);
-            std::string token;
+            std::string title;
+            std::getline(ss, title); // read "begin"
 
-            while (std::getline(ss, token, ','))
-            { // Tách dữ liệu bằng dấu phẩy
-                if (token == "Begin" || token == "N")
-                {
-                    std::cout << "Value: " << token << ", Type: " << typeid(token).name() << std::endl;
-                }
-                else
-                {
-                    try
-                    {
-                        int ok = 1;
-                        int intValue = std::stoi(token); // Chuyển đổi thành số nguyên
-                        std::cout << "Value: " << intValue + ok << ", Type: " << typeid(intValue).name() << std::endl;
-                    }
-                    catch (...)
-                    {
-                        std::cerr << "Error converting string to integer!" << std::endl;
-                    }
-                }
+            std::getline(file, line);
+            std::stringstream ss2(line);
+            std::string numTasksStr;
+            std::getline(ss2, numTasksStr);
+            int numTasks = std::stoi(numTasksStr);
+
+            std::getline(file, line);
+            std::stringstream ss3(line);
+            std::string numProcessorsStr;
+            std::getline(ss3, numProcessorsStr);
+            int numProcessors = std::stoi(numProcessorsStr);
+
+            std::getline(file, line);
+            std::stringstream ss4(line);
+            std::string preemptive;
+            std::getline(ss4, preemptive);
+
+            TaskSet taskSet(numTasks, numProcessors);
+            for (int i = 0; i < numTasks; i++)
+            {
+                std::getline(file, line);
+                std:: string startTime;
+                std:: string computationTime;
+                std:: string softDeadline;
+                std:: string hardDeadline;
+                std:: string period;
+                std::stringstream ss5(line);
+                std::getline(ss5, startTime, ',');
+                std::getline(ss5, computationTime, ',');
+                std::getline(ss5, softDeadline, ',');
+                std::getline(ss5, hardDeadline, ',');
+                std::getline(ss5, period, ',');
+                Task task("Task" + std::to_string(i), std::stoi(startTime), std::stoi(computationTime), std::stoi(hardDeadline), std::stoi(period));
+                taskSet.addTask(task);
             }
-            std::cout << " new" << endl;
+            taskSetList.push_back(taskSet);
         }
-        file.close(); // Đóng file
+
+        for (int i = 0; i < taskSetList.size(); i++)
+        {
+            TaskSet taskSet = taskSetList[i];
+            cout << "TaskSet " << i << endl;
+            cout << "NumTasks: " << taskSet.getNumTasks() << endl;
+            cout << "NumProcessors: " << taskSet.getNumProcessors() << endl;
+            
+        }
+
+        
     }
     else
     {
         std::cerr << "Unable to open file!" << std::endl;
     }
 
-    // input.close();
 
     return 0;
 }
