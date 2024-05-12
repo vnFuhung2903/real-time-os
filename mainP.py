13f3c18b7ed2c904b1bb36b7821c8a29f56fc557
ndthangit <ndtducthang2004@gmail.com>
2024-05-12 09:55:12 +0700

    update ui

--- mainP.py
+++ mainP.py
@@ -3,15 +3,21 @@ import tkinter as tk
 from matplotlib.backends.backend_tkagg import FigureCanvasTkAgg
 import subprocess
 import pandas as pd
+import numpy as np
+import os
 
 
 
 # compile the c++ code
-subprocess.call(["g++", "main.cpp", "EdfAlgorithm.cpp","-o", "main"])
-subprocess.call(["./main"])
-
+# subprocess.call(["g++", "main.cpp", "EdfAlgorithm.cpp","-o", "main"])
+# subprocess.call(["./main"])
+#
 data = pd.read_csv("Result.csv")
 
+root = tk.Tk()
+root.title("Controller")
+label = tk.Label(root, text="control panel", font=("Arial", 24), fg="blue" ,padx=10, pady=10)
+label.pack(pady=5)  # Add vertical padding
 
 def create_new_window1():
     new_window = tk.Toplevel()
@@ -65,45 +71,17 @@ def create_new_window2():
     canvas.get_tk_widget().pack(side=tk.TOP, fill=tk.BOTH, expand=1)
 
 def create_new_window3():
-    new_window = tk.Toplevel()
-    new_window.title("Comparison tables")
-    new_window.geometry("800x600")
-    # create an image by matplotlib
-    fig, ax = plt.subplots(figsize=(10, 6))
+    os.system('start cmd /k python EDF.py')
 
-    # Plot the data
-    ax.plot(data['TaskSet'], data['NtEDF'], marker='o')
-    ax.plot(data['TaskSet'], data['NtEDFminT'], marker='o')
-    ax.plot(data['TaskSet'], data['NtEDFmaxT'], marker='o')
-    ax.plot(data['TaskSet'], data['NtEDFminOP'], marker='o')
-    ax.plot(data['TaskSet'], data['NtEDFmaxOP'], marker='o')
+button1 = tk.Button(root, text="Comparison time processes", command=create_new_window1, width=30, height=3, wraplength=200)
+button1.pack(pady=5)
 
-    # Set the labels and title
-    ax.set_xlabel('TaskSet')
-    ax.set_ylabel('num Tasks')
-    ax.set_title('The number of tasks across deadline ')
-    ax.grid(True)
+button2 = tk.Button(root, text="Comparison number of off deadline processes", command=create_new_window2, width=30, height=3, wraplength=200)
+button2.pack(pady=5)
 
-    # Create a new canvas and add it to the window
-    canvas = FigureCanvasTkAgg(fig, master=new_window)
-    canvas.draw()
-    canvas.get_tk_widget().pack(side=tk.TOP, fill=tk.BOTH, expand=1)
+button3 = tk.Button(root, text="processes management", command=create_new_window3, width=30, height=3, wraplength=200)
+button3.pack(pady=5)
 
-
-root = tk.Tk()
-label = tk.Label(root, text="control panel", font=("Arial", 24), fg="blue" ,padx=10, pady=10)
-label.pack(pady=10)  # Add vertical padding
-
-button1 = tk.Button(root, text="Comparison time processes", command=create_new_window1)
-button1.pack(pady=10)  # Add vertical padding
-
-button2 = tk.Button(root, text="Comparison number of off deadline processes", command=create_new_window2)
-button2.pack(pady=10)  # Add vertical padding
-
-button3 = tk.Button(root, text="processes management", command=create_new_window3)
-button3.pack(pady=10)  # Add vertical padding
-
-buttonQuit = tk.Button(root, text="Quit", command=root.quit)
-buttonQuit.pack(pady=10)  # Add vertical padding
-
+buttonQuit = tk.Button(root, text="Quit", command=root.quit, width=30, height=3, wraplength=200)
+buttonQuit.pack(pady=5)
 root.mainloop()
